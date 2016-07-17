572965b397e8f2f846d963a5d4f6dd146a912db5
Stanislav <stanislav@pixsens.co.uk>
2016-07-17 10:46:49 +0100

    fixed youtube video

--- resources/views/frontend/pages/show-song.blade.php
+++ resources/views/frontend/pages/show-song.blade.php
@@ -14,7 +14,9 @@
                 <h1>{{ $song->title }} <a href="{{ action('FrontendController@editSong', ['id' => $song->id]) }}" class="btn btn-default"><i class="fa fa-edit"></i> Edytuj</a></h1>
                 <div class="youtube-preview">
                     @if($song->sample)
-                        <iframe width="560" height="315" src="{{ str_replace("watch?v=", "embed/", $song->sample) }}" frameborder="0" allowfullscreen></iframe>
+                        <div class="videoWrapper">
+                            <iframe width="560" height="315" src="{{ str_replace("watch?v=", "embed/", $song->sample) }}" frameborder="0" allowfullscreen></iframe>
+                        </div>
                     @endif
                 </div>
                 @if($song->chords)
@@ -45,3 +47,42 @@
     </section>
     @include('footer')
 @stop
+@push('scripts')
+<script>
+    // Find all YouTube videos
+    var $allVideos = $("iframe[src^='//www.youtube.com']"),
+
+    // The element that is fluid width
+            $fluidEl = $("body");
+
+    // Figure out and save aspect ratio for each video
+    $allVideos.each(function() {
+
+        $(this)
+                .data('aspectRatio', this.height / this.width)
+
+                // and remove the hard coded width/height
+                .removeAttr('height')
+                .removeAttr('width');
+
+    });
+
+    // When the window is resized
+    $(window).resize(function() {
+
+        var newWidth = $fluidEl.width();
+
+        // Resize all videos according to their own aspect ratio
+        $allVideos.each(function() {
+
+            var $el = $(this);
+            $el
+                    .width(newWidth)
+                    .height(newWidth * $el.data('aspectRatio'));
+
+        });
+
+// Kick off one resize to fix all videos on page load
+    }).resize();
+</script>
+@end
